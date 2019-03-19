d674f88b7075cef570a58e5b2981221130fd2662
Marcus Mellor <marcus@mellor.com>
2019-03-19 11:01:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves( array )
   # Your code here
+  array.each_with_index {|name, index| puts "#{index+1} #{name}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet( array )
   # Your code here
+   array.collect{|planet| planet.capitalize+"!" }
 end
 
 def long_planeteer_calls# code an argument here
