aecd8fde54044f5d153a6c04afb4841306d88467
Marcus Mellor <marcus@mellor.com>
2019-03-19 11:04:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,11 @@ def summon_captain_planet( array )
    array.collect{|planet| planet.capitalize+"!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls( array )
   # Your code here
+  array.any? do |x|
+    x.length>4
+  end
 end
 
 def find_the_cheese# code an argument here
